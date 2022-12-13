9dd709d47282b82c352f47c37cd387927aaa23c4
GusAdolf <guta.contre@hotmail.com>
2022-12-12 23:27:24 -0500

    inicio del proyecto

---
+++ front.py
@@ -0,0 +1,25 @@
+#IMPORTAR LIBRERIA PARA USAR FRAMEWORK FLASK
+from flask import Flask
+from flask import render_template
+import os
+from flask import request
+import backend
+##llamado a flask
+app = Flask(__name__)
+
+##servicio web
+@app.route('/', methods = ["GET","POST"])
+def home():
+    return render_template('index.html')
+
+@app.route('/cursos', methods = ["GET","POST"])
+def cursos():
+    return render_template('cursos.html')
+
+
+
+
+##ejecutar el servicio web
+if __name__=='__main__':
+    #OJO QUITAR EL DEBUG EN PRODUCCION
+    app.run(host='0.0.0.0', port=5000, debug=True)
